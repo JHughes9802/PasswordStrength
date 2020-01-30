fb655fe5aefba78fa7adc93c09c2a8c1eee8b000
Owner <ij5775wy@go.minneapolis.edu>
2020-01-30 12:39:18 -0600

    Add project files.

---
+++ PasswordStrength/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace PasswordStrength
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnEstimate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Example Password:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(81, 95);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(69, 33);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "label 2";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResult.Visible = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(139, 18);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(137, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnEstimate
+            // 
+            this.btnEstimate.Location = new System.Drawing.Point(84, 60);
+            this.btnEstimate.Name = "btnEstimate";
+            this.btnEstimate.Size = new System.Drawing.Size(113, 23);
+            this.btnEstimate.TabIndex = 3;
+            this.btnEstimate.Text = "Estimate Strength";
+            this.btnEstimate.UseVisualStyleBackColor = true;
+            this.btnEstimate.Click += new System.EventHandler(this.btnEstimate_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEstimate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 137);
+            this.Controls.Add(this.btnEstimate);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Password Strength Estimator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnEstimate;
+    }
+}
+
---
+++ PasswordStrength/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordStrength
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEstimate_Click(object sender, EventArgs e)
+        {
+            lblResult.Show();
+            string password = txtPassword.Text;
+            int strength = 0;
+            int hasUpper = 0;
+            int hasLower = 0;
+            int hasSymbol = 0;
+            int hasDigit = 0;
+            int longEnough = 0;
+
+            if (password.Any(char.IsUpper))
+            {
+                hasUpper = 1;
+            }
+            if (password.Any(char.IsLower))
+            {
+                hasLower = 1;
+            }
+            if (password.Any(char.IsDigit))
+            {
+                hasDigit = 1;
+            }
+            if (password.Any(char.IsPunctuation))
+            {
+                hasSymbol = 1;
+            }
+            if (password.Length >= 8)
+            {
+                longEnough = 1;
+            }
+            
+            strength = hasUpper + hasLower + hasSymbol + hasDigit + longEnough;
+
+            if(strength == 5)
+            {
+                lblResult.Text = "Strong";
+                lblResult.BackColor = Color.LightGreen;
+            }
+            else if (strength >=3)
+            {
+                lblResult.Text = "Medium";
+                lblResult.BackColor = Color.Yellow;
+            }
+            else
+            {
+                lblResult.Text = "Weak";
+                lblResult.BackColor = Color.Red;
+            }
+        }
+    }
+}
